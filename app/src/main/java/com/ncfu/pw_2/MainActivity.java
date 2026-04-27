032709bfbf1c4e27c0d90b740b31890d69b2c5d8
KI7ANA <sergej.tkachev.02@bk.ru>
2026-04-27 09:05:38 +0900

    Add Android project source files for PR2

---
+++ app/src/main/java/com/ncfu/pw_2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ncfu.pw_2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_variant5);
+    }
+}
